e8dbc65d853ee88a38299a26e915b74e51da6932
thierry le gall <thierry28220@live.fr>
2023-03-06 16:00:14 +0100

    ok

---
+++ src/Form/ChevauxCoursesType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Chevaux;
+use App\Entity\ChevauxCourses;
+use App\Entity\Courses;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChevauxCoursesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            //->add('resultat')
+            //->add('leCheval')
+            ->add('leCheval', EntityType::class, array(
+                'class'        => Chevaux::class,
+                'choice_label' => 'nomCheval',
+                'multiple' => false  
+            ))
+
+            //->add('laCourse')
+            ->add('laCourse', EntityType::class, array(
+                'class'        => Courses::class,
+                'choice_label' => 'nomCourse',
+                'multiple' => false  
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ChevauxCourses::class,
+        ]);
+    }
+}
